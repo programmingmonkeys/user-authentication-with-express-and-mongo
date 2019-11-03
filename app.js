e65fa94a1c65aa32535f48c9f27457acc89f6539
codingChewie <matthewtimothy910@yahoo.com>
2019-11-03 09:09:11 -0600

    Creating a Login Form With Pug (Jade)

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.set('views', path.join(__dirname, '/views'))
 // include routes
 const routes = require('./routes/index')
 
+// mount routes
 app.use('/', routes)
 
 // catch 404 and forward to error handler
