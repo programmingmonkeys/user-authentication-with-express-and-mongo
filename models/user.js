73430cd24d72bbe85da4a2ae0732597b0b41e0bb
codingChewie <matthewtimothy910@yahoo.com>
2019-11-02 22:36:02 -0500

    Defining a Mongo Schema with Mongoose

---
+++ models/user.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  favoriteBook: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+// authenticate input against database documents
+UserSchema.statics.authenticate = function(email, password, callback) {
+  this.findOne({ email }).exec(function(error, user) {
+    if (error) return callback(error)
+
+    if (!user) {
+      const err = new Error('User not found.')
+      err.status = 401
+      return callback(err)
+    }
+
+    bcrypt.compare(password, user.password, (error, result) => {
+      if (result === true) return callback(null, user)
+
+      return callback()
+    })
+  })
+}
+
+// hash password before saving to database
+UserSchema.pre('save', function(next) {
+  const user = this
+
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) return next(err)
+
+    user.password = hash
+    next()
+  })
+})
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
