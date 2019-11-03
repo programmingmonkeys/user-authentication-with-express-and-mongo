e5a60648790d3f6b8aad30e01608d55ada1a1626
codingChewie <matthewtimothy910@yahoo.com>
2019-11-03 09:00:09 -0600

    Working with Sessions in Express

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
+const session = require('express-session')
 
 const app = express()
+
+// use sessions for tracking logins
+app.use(
+  session({
+    secret: 'learning soemthing',
+    resave: true,
+    saveUninitialized: false,
+  }),
+)
 
 // mongodb connection
 mongoose.connect('mongodb://localhost:27017/bookworm', {
